147ad7e084bfb1fa5838c6bd10b289d3e2da2208
RinasPlay <rinas2006bik@gmail.com>
2026-02-19 12:47:27 +0300

    новые свойства и базовые структуры

--- Enterprise.cs
+++ Enterprise.cs
@@ -1,45 +1,35 @@
-// TODO:
-// 1. Добавить свойство для типа предприятия (торговля, производство, офис)
-// 2. Реализовать проверку корректности данных (адрес, этажность)
-// 3. Реализовать информативное строковое представление предприятия
-
 namespace FireInspection
 {
     public class Enterprise
     {
-        public int Id { get; set; }                    // Идентификатор
-        public string Name { get; set; }               // Название предприятия
-        public string Address { get; set; }            // Адрес
-        public int FloorCount { get; set; }            // Количество этажей
-        public int EmployeeCount { get; set; }         // Количество сотрудников
-        
-        // TODO 1: Добавить свойство EnterpriseType (тип предприятия)
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int FloorCount { get; set; }
+        public int EmployeeCount { get; set; }
+
+        // ADDED IN TODO 1
+        public string EnterpriseType { get; set; }
+
         public Enterprise(int id, string name, string address, int floors, int employees, string type)
         {
             Id = id;
             Name = name;
             Address = address;
-            
-            // TODO 2: Проверить что количество этажей не отрицательное
-            // Если floors < 1, установить 1
-            
-            // TODO 2: Проверить что количество сотрудников не отрицательное
-            // Если employees < 0, установить 0
-            
-            // TODO 1: Сохранить тип предприятия
+
+            // TODO 2 будет добавлен позже (проверки)
+            FloorCount = floors;
+            EmployeeCount = employees;
+
+            // ADDED IN TODO 1: сохраняем тип
+            EnterpriseType = type;
         }
-        
-        public override string ToString()
-        {
-            // TODO 3: Вернуть строку в формате "ООО 'Ромашка' (торговля) - ул. Ленина, 10, 3 этажа, 50 сотрудников"
-            return Name;
-        }
-        
-        // Определить категорию риска
+
+        // Заглушка для TODO 3
+        public override string ToString() { return Name; }
+
         public string GetRiskCategory()
         {
-            // Готовый метод: определить категорию по количеству сотрудников и этажей
             if (EmployeeCount > 100 || FloorCount > 5) return "Высокая";
             if (EmployeeCount > 50 || FloorCount > 3) return "Средняя";
             return "Низкая";
