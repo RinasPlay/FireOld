da5a163eafda620343cd578f8c8b520d72f64a5e
RinasPlay <rinas2006bik@gmail.com>
2026-02-19 12:19:14 +0300

    Добавлено: Свойства ViolationType и Severity. Сохранение этих значений в конструкторе.

--- Violation.cs
+++ Violation.cs
@@ -1,61 +1,47 @@
-// TODO:
-// 1. Реализовать систему классификации нарушений
-// 2. Реализовать оценку серьезности нарушения
-// 3. Реализовать систему учета сроков устранения
-
 using System;
 
 namespace FireInspection
 {
     public class Violation
     {
-        public int Id { get; set; }                    // Номер нарушения
-        public string Description { get; set; }        // Описание нарушения
-        public DateTime DetectionDate { get; set; }    // Дата обнаружения
-        public DateTime FixDeadline { get; set; }      // Срок устранения
-        
-        // TODO 1: Добавить свойство ViolationType (тип: пожарные выходы, огнетушители, проводка и т.д.)
-        // TODO 1: Добавить свойство Severity (серьезность: критическое, значительное, незначительное)
-        
-        public bool IsFixed { get; set; }              // Устранено ли нарушение
-        public DateTime? FixDate { get; set; }         // Дата устранения (если устранено)
-        
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime DetectionDate { get; set; }
+        public DateTime FixDeadline { get; set; }
+
+        // Добавлено
+        public string ViolationType { get; set; }
+        public string Severity { get; set; }
+
+        public bool IsFixed { get; set; }
+        public DateTime? FixDate { get; set; }
+
         public Violation(int id, string description, string type, string severity, DateTime detectionDate, int daysToFix)
         {
             Id = id;
             Description = description;
             DetectionDate = detectionDate;
-            
-            // TODO 1: Сохранить тип и серьезность нарушения
-            
-            // TODO 3: Установить срок устранения (DetectionDate + daysToFix дней)
-            
+
+            // Сохранение типа и серьёзности
+            ViolationType = type;
+            Severity = severity;
+
+            // TODO 3 будет реализован позже
+            // FixDeadline = DetectionDate.AddDays(daysToFix);
+
             IsFixed = false;
         }
-        
-        // TODO 2: Получить приоритет исправления
-        public int GetFixPriority()
-        {
-            // Вернуть числовой приоритет на основе серьезности и просрочки
-            // Критическое + просрочено = 1 (самый высокий)
-            // Незначительное + не просрочено = 3 (низкий)
-            return 1;
-        }
-        
-        // TODO 3: Проверить просрочку
-        public bool IsOverdue()
-        {
-            // Вернуть true если текущая дата позже FixDeadline и нарушение не устранено
-            return false;
-        }
-        
-        // Отметить как устраненное
+
+        public int GetFixPriority() => 1; // пока заглушка
+
+        public bool IsOverdue() => false; // пока заглушка
+
         public void MarkAsFixed()
         {
             IsFixed = true;
             FixDate = DateTime.Now;
         }
-        
+
         public override string ToString()
         {
             string status = IsFixed ? "Устранено" : "Не устранено";
