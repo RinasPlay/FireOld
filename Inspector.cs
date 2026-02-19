f78dd3d3bbc92b0701428e508361eecb69c745ee
RinasPlay <rinas2006bik@gmail.com>
2026-02-19 12:50:38 +0300

    новые свойства и базовые структуры

--- Inspector.cs
+++ Inspector.cs
@@ -1,8 +1,3 @@
-// TODO:
-// 1. Реализовать учет данных инспектора
-// 2. Реализовать статистику работы инспектора
-// 3. Реализовать систему специализации инспектора
-
 using System.Collections.Generic;
 
 namespace FireInspection
@@ -11,46 +6,35 @@ namespace FireInspection
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string BadgeNumber { get; set; }        // Номер удостоверения
-        
-        // TODO 1: Добавить свойство Rank (звание: инспектор, старший инспектор, главный инспектор)
-        // TODO 1: Добавить свойство Specialization (специализация: промышленность, торговля, жилье)
-        
+        public string BadgeNumber { get; set; }
+
+        // ADDED IN TODO 1
+        public string Rank { get; set; }
+        public string Specialization { get; set; }
+
         private List<Inspection> conductedInspections = new List<Inspection>();
-        
-        // TODO 2: Добавить проведенную проверку
-        public void AddInspection(Inspection inspection)
-        {
-            // Добавить проверку в список conductedInspections
-        }
-        
-        // TODO 3: Получить статистику работы
+
+        // Заглушка для TODO 2
+        public void AddInspection(Inspection inspection) { }
+
+        // Заглушка для TODO 3
         public void GetWorkStats(out int totalInspections, out int totalViolations, out decimal avgSafetyScore)
         {
             totalInspections = conductedInspections.Count;
             totalViolations = 0;
             avgSafetyScore = 0;
-            
-            // Пройти по всем проверкам, посчитать общее количество нарушений
-            // Рассчитать средний балл безопасности
         }
-        
-        // TODO 3: Получить количество активных нарушений
-        public int GetActiveViolationsCount()
-        {
-            int count = 0;
-            
-            // Пройти по всем проверкам и всем нарушениям
-            // Посчитать нарушения, которые еще не устранены
-            return count;
-        }
-        
-        // Показать информацию об инспекторе
+
+        // Заглушка для TODO 3
+        public int GetActiveViolationsCount() { return 0; }
+
         public void ShowInspectorInfo()
         {
             Console.WriteLine($"Инспектор: {Name}");
             Console.WriteLine($"Удостоверение: {BadgeNumber}");
-            // TODO 1: Вывести звание и специализацию
+            // ADDED IN TODO 1: вывод звания и специализации
+            Console.WriteLine($"Звание: {Rank ?? "не указано"}");
+            Console.WriteLine($"Специализация: {Specialization ?? "не указана"}");
             Console.WriteLine($"Проведено проверок: {conductedInspections.Count}");
         }
     }
