8e56f042ff48534994ae9e521c4d38f164fd3c05
RinasPlay <rinas2006bik@gmail.com>
2026-02-19 12:32:28 +0300

    Начало

--- Violation.cs
+++ Violation.cs
@@ -1,20 +1,24 @@
+// TODO:
+// 1. Реализовать систему классификации нарушений
+// 2. Реализовать оценку серьезности нарушения
+// 3. Реализовать систему учета сроков устранения
+
 using System;
 
 namespace FireInspection
 {
     public class Violation
     {
-        public int Id { get; set; }
-        public string Description { get; set; }
-        public DateTime DetectionDate { get; set; }
-        public DateTime FixDeadline { get; set; }
+        public int Id { get; set; }                    // Номер нарушения
+        public string Description { get; set; }        // Описание нарушения
+        public DateTime DetectionDate { get; set; }    // Дата обнаружения
+        public DateTime FixDeadline { get; set; }      // Срок устранения
 
-        // Добавлено
-        public string ViolationType { get; set; }
-        public string Severity { get; set; }
+        // TODO 1: Добавить свойство ViolationType (тип: пожарные выходы, огнетушители, проводка и т.д.)
+        // TODO 1: Добавить свойство Severity (серьезность: критическое, значительное, незначительное)
 
-        public bool IsFixed { get; set; }
-        public DateTime? FixDate { get; set; }
+        public bool IsFixed { get; set; }              // Устранено ли нарушение
+        public DateTime? FixDate { get; set; }         // Дата устранения (если устранено)
 
         public Violation(int id, string description, string type, string severity, DateTime detectionDate, int daysToFix)
         {
@@ -22,20 +26,30 @@ namespace FireInspection
             Description = description;
             DetectionDate = detectionDate;
 
-            // Сохранение типа и серьёзности
-            ViolationType = type;
-            Severity = severity;
+            // TODO 1: Сохранить тип и серьезность нарушения
 
-            // TODO 3 будет реализован позже
-            // FixDeadline = DetectionDate.AddDays(daysToFix);
+            // TODO 3: Установить срок устранения (DetectionDate + daysToFix дней)
 
             IsFixed = false;
         }
 
-        public int GetFixPriority() => 1; // пока заглушка
+        // TODO 2: Получить приоритет исправления
+        public int GetFixPriority()
+        {
+            // Вернуть числовой приоритет на основе серьезности и просрочки
+            // Критическое + просрочено = 1 (самый высокий)
+            // Незначительное + не просрочено = 3 (низкий)
+            return 1;
+        }
 
-        public bool IsOverdue() => false; // пока заглушка
+        // TODO 3: Проверить просрочку
+        public bool IsOverdue()
+        {
+            // Вернуть true если текущая дата позже FixDeadline и нарушение не устранено
+            return false;
+        }
 
+        // Отметить как устраненное
         public void MarkAsFixed()
         {
             IsFixed = true;
